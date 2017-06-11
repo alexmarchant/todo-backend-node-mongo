e48a98cfcb05115013e21cba7c2f683f1ab1e479
Alex Marchant <alexjmarchant@gmail.com>
2017-06-11 00:57:05 -0400

    Enable cross origin scripting

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ mongoose.connect(mongoURI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 routes(app);
 
