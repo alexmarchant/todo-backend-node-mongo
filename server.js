bbdca6bd7daecab2f2794f4529e73c95e7b1c20d
Alex Marchant <alexjmarchant@gmail.com>
2017-06-11 01:30:19 -0400

    CORS methods

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ mongoose.connect(mongoURI);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
