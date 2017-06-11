2da28ca2131d804467fd80c1fa493ce380979602
Alex Marchant <alexjmarchant@gmail.com>
2017-06-11 01:09:29 -0400

    Formatting

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/todo-backend';
 mongoose.promise = global.Promise;
 mongoose.connect(mongoURI);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
