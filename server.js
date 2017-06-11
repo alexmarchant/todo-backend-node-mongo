c708cbae3458dff7b63fcc2ce487bec7cb4ca149
Alex Marchant <alexjmarchant@gmail.com>
2017-06-11 02:59:16 -0400

    Enable patch

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 // Allow cross origin requests
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   //intercepts OPTIONS method
